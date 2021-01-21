9235700196e106a6073cead15ee8d93db2ba33d8
John Gillan <jgillan@protonmail.com>
2021-01-21 15:39:17 +0100

    changed colour

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import Results from './Results';
-
-
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Results from "./Results";
 
 class Home extends Component {
-  resultsDisplay = () => {
-    return (this.props.globalState.status === '' ? '' : <Results globalState={this.props.globalState}/>)
+    resultsDisplay = () => {
+        return this.props.globalState.status === "" ? (
+            ""
+        ) : (
+            <Results globalState={this.props.globalState} />
+        );
+    };
+
+    render() {
+        return (
+            <section className="">
+                <h2 className="main-heading">Bitcoin Profit Tracker</h2>
+                <div className="row">
+                    <div className="col-md-6 left">
+                        <h2 id="instructions">
+                            Enter the number of bitcoins bought and the date of
+                            the transaction to find total profit or loss
+                        </h2>
+                        <label>Price</label>
+                        <input
+                            onChange={this.props.inputChangeHandler}
+                            value={this.props.globalState.cryptoAmount}
+                            type="number"
+                            name="price"
+                        />
+                        <label>Date</label>
+                        <DatePicker
+                            selected={this.props.globalState.date}
+                            onChange={this.props.handleDateChange}
+                        />
+                        <button
+                            onClick={this.props.apiCall}
+                            className="btn"
+                            type="submit"
+                        >
+                            Check
+                        </button>
+                    </div>
+                    {this.resultsDisplay()}
+                </div>
+            </section>
+        );
     }
-  
-  render () {
-    return (
-      <section className="">
-      <div className="row">
-      <div className="ads"></div>
-        <div className="col-md-6 left">
-        <h2 id="instructions">Enter the number of bitcoins bought and the date of the transaction to find total profit or loss</h2>
-          <label>Price</label>
-          <input onChange={this.props.inputChangeHandler} value={this.props.globalState.cryptoAmount} type="number" name="price"/>
-          <label>Date</label>
-          <DatePicker 
-            selected={this.props.globalState.date}
-            onChange={this.props.handleDateChange} 
-          />
-          <button onClick={this.props.apiCall} className="btn" type="submit">Check</button>
-        </div>
-        {this.resultsDisplay()}
-        <div className="ads"></div>
-        </div>
-      </section>
-    );
-  }
 }
 
 export default Home;
